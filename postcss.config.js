113eb615f4ef56da6e4e30783d422168bb633a11
Alex Bruch <alex.bruch@tuta.com>
2025-07-02 00:00:39 +0200

    update visuals

--- postcss.config.js
+++
@@ -1,8 +0,0 @@
-// postcss.config.js
-module.exports = {
-  plugins: {
-    // Change 'tailwindcss' to '@tailwindcss/postcss'
-    '@tailwindcss/postcss': {},
-    autoprefixer: {},
-  },
-};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: ["./index.html"],
-  darkMode: ['media'],
+  safelist: ['font-inter'],
+  darkMode: ['class'],
   theme: {
     extend: {},
   },
